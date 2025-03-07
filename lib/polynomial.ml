6a98a8d050f143b04cafa6e2244278881f9de9e7
Nathan Mull <nmmull@gmail.com>
2025-03-07 13:42:00 -0500

    updates

--- lib/polynomial.ml
+++ lib/polynomial.ml
@@ -8,3 +8,7 @@ let monomial coeff exp = M.singleton exp coeff
 let line slope = monomial slope 1
 
 let num_terms poly = M.cardinal poly
+
+let pp : t Fmt.t = Fmt.nop (* TODO *)
+
+let equal = M.equal Float.equal
--- test/test_polynomial.ml
+++ test/test_polynomial.ml
@@ -1,20 +1,30 @@
-open Compsig.Polynomial
 open Alcotest
+
+module P = Compsig.Polynomial
+
+let poly = testable P.pp P.equal
 
 let test_num_terms_empty () =
   check int
     "empty has no terms"
     0
-    (num_terms empty)
+    P.(num_terms empty)
 
 let test_num_terms_monomial () =
   check int
     "monomial has one terms"
     1
-    (num_terms (monomial 23.4 40))
+    P.(num_terms (monomial 23.4 40))
+
+let test_dumb () =
+  check poly
+    "empty is empty"
+    P.empty
+    P.(monomial 23.4 40)
 
 let tests =
   [
     test_case "empty has 0 terms" `Quick test_num_terms_empty;
     test_case "monomial has 1 term" `Quick test_num_terms_monomial;
+    test_case "empty is empty" `Quick test_dumb;
   ]
