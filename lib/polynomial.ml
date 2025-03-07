026cfb00f949ad421e0124e20824a639c506e3f3
Nathan Mull <nmmull@gmail.com>
2025-03-07 12:57:42 -0500

    added basic poly

---
+++ lib/polynomial.ml
@@ -0,0 +1,8 @@
+module M = Map.Make(Int)
+
+type t = float M.t (* map exponents to coefficients *)
+
+let empty = M.empty
+let const value = M.singleton 0 value
+let monomial coeff exp = M.singleton exp coeff
+let line slope = monomial slope 1
