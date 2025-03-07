f1a4e5270dcf4abb586c94620c6a8995f241e854
Nathan Mull <nmmull@gmail.com>
2025-03-07 13:16:33 -0500

    added a second test

--- test/test_polynomial.ml
+++ test/test_polynomial.ml
@@ -1,13 +1,20 @@
 open Compsig.Polynomial
 open Alcotest
 
-let test_empty () =
+let test_num_terms_empty () =
   Alcotest.(check int)
     "empty has no terms"
     0
     (num_terms empty)
 
+let test_num_terms_monomial () =
+  Alcotest.(check int)
+    "monomial has one terms"
+    1
+    (num_terms (monomial 23.4 40))
+
 let tests =
   [
-    test_case "empty" `Quick test_empty;
+    test_case "empty has 0 terms" `Quick test_num_terms_empty;
+    test_case "monomial has 1 term" `Quick test_num_terms_monomial;
   ]
