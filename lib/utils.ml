7fbbf58498728d7e0bafbceb4527da10ab0cb082
William Chen <chenxw@bu.edu>
2025-04-02 13:55:40 -0400

    added int_to_exponent

--- lib/utils.ml
+++ lib/utils.ml
@@ -68,3 +68,31 @@ module IntCoefficient = struct
   include Int
   let pp = Fmt.nop
 end
+
+let int_to_exponent (input : int) : string =
+  let rec go output_string = function
+    | 0 -> output_string
+    | num -> 
+      let take_value value =
+        let ones = value mod 10 in
+        let other = value - ones in
+        let find_exp n = 
+          match n with
+          | 0 -> "⁰"
+          | 1 -> "¹"
+          | 2 -> "²"
+          | 3 -> "³"
+          | 4 -> "⁴"
+          | 5 -> "⁵"
+          | 6 -> "⁶"
+          | 7 -> "⁷"
+          | 8 -> "⁸"
+          | 9 -> "⁹"
+          | _ -> assert false
+        in
+        find_exp ones, other
+      in
+      match take_value num with
+      | (expon, other) -> go (expon ^ output_string) (other / 10)
+  in
+  go "" input
