7271cc574b8f998d13d65707d37883f912cedeb3
William Chen <chenxw@bu.edu>
2025-04-11 12:02:43 -0400

    slight indentation fixes in signal.ml

--- lib/signal.ml
+++ lib/signal.ml
@@ -3,26 +3,26 @@ type 'a base_signal =
   | Sin of 'a
 
 module rec P : Polynomial_intf.POLYNOMIAL
-           with type coefficient = float
-           with type base = Base.t
-           with type monomial = Monomial.Make(Base).t
+          with type coefficient = float
+          with type base = Base.t
+          with type monomial = Monomial.Make(Base).t
   = Polynomial.Make(Utils.FloatCoefficient)(Base)
-   and Base : Utils.BASE with type t = P.t base_signal = struct
-     type t = P.t base_signal
+    and Base : Utils.BASE with type t = P.t base_signal = struct
+  type t = P.t base_signal
 
-     let compare b1 b2 =
-       match b1, b2 with
-       | Ident, Ident -> 0
-       | Ident, _ -> -1
-       | _, Ident -> 1
-       | Sin p1, Sin p2 -> P.compare p1 p2
+  let compare b1 b2 =
+    match b1, b2 with
+      | Ident, Ident -> 0
+      | Ident, _ -> -1
+      | _, Ident -> 1
+      | Sin p1, Sin p2 -> P.compare p1 p2
 
-     let to_string = function
-     | Ident -> "t"
-     | Sin signal -> "sin(" ^ Fmt.to_to_string P.pp signal ^ ")"
+  let to_string = function
+    | Ident -> "t"
+    | Sin signal -> "sin(" ^ Fmt.to_to_string P.pp signal ^ ")"
 
-     let pp = Fmt.of_to_string to_string
-   end
+  let pp = Fmt.of_to_string to_string
+end
 
 include P
 module M = Monomial.Make(Base)
@@ -33,8 +33,8 @@ let const = P.of_coefficient
 
 let rec comp_base base signal =
   match base with
-  | Ident -> signal
-  | Sin p1 -> P.of_base (Sin (P.comp comp_base p1 signal))
+    | Ident -> signal
+    | Sin p1 -> P.of_base (Sin (P.comp comp_base p1 signal))
 
 let comp = P.comp comp_base
 
@@ -42,10 +42,10 @@ let rec to_expr (s : t) =
   s
   |> P.to_list
   |> List.map (fun (c, m) ->
-         let m = monomial_to_expr m in
-         if c = 1.
-         then m
-         else Syntax.Prod [Syntax.Const c; m])
+        let m = monomial_to_expr m in
+          if c = 1.
+            then m
+          else Syntax.Prod [Syntax.Const c; m])
   |> List.filter ((<>) (Syntax.Const 0.))
   |> fun l -> Syntax.Sum l
 and monomial_to_expr mono =
